60132dfbdeaaebeca995dc1fd4ed641aaec0356e
DangNguyenTranNgoc <dangnguyentranngoc@gmail.com>
2023-06-08 23:40:52 +0700

    Optimize scripts and playbook for Docker

--- script/docker-import-app.sql
+++
@@ -1,7 +0,0 @@
-load data local infile '/usr/local/share/data/app.csv'
-into table app
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(App, Category, Rating, Reviews, Size, Installs, Type, Price, `Content Rating`, Genres, `Last Updated`, `Current Ver`, `Android Ver`);
